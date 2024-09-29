c5aff8cddf30c8ebdd9365daf4127c782a26b7c5
faurdent <nikitalukianitcares@gmail.com>
2024-09-29 19:21:01 +0200

    Mainnet address added, logic fixed, middleware added

--- web_app/api/main.py
+++ web_app/api/main.py
@@ -4,6 +4,7 @@ from uuid import uuid4
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from starlette.middleware.cors import CORSMiddleware
 from starlette.middleware.sessions import SessionMiddleware
 
 from web_app.api.dashboard import router as dashboard_router
@@ -21,6 +22,8 @@ app.mount("/static", StaticFiles(directory="web_app/static"), name="static")
 
 # Add session middleware with a secret key
 app.add_middleware(SessionMiddleware, secret_key=f"Secret:{str(uuid4())}")
+# CORS middleware for React frontend
+app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_headers=["*"], allow_methods=["*"])
 
 # Include the form and login routers
 app.include_router(form_router)
